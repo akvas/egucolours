7b3d9e322b9ab5eb2861b144770e62f21070ce7f
Andreas Kvas <andreas@kvas.at>
2022-11-19 22:29:24 +0100

    added dictionaries with hex color strings

---
+++ egucolours/_color_data.py
@@ -0,0 +1,9 @@
+# Main and Neutral palettes taken from https://www.egu.eu/visual-identity/
+MAIN_PALETTE = {'blue': '#0072BC', 'yellow': '#FFDE00'}
+NEUTRAL_PALETTE = {'white': '#ffffff', 'gray': '#A8A9AD'}
+
+# Division colors taken from https://www.egu.eu/structure/divisions/
+DIVISION_COLOURS = {'AS': '#4242d4', 'BG': '#1a4d04', 'CL': '#5f3172', 'CR': '#007db8', 'EMRP': '#87251f', 'ERE': '#2a6736',
+                   'ESSI': '#6a4a96', 'G': '#a450ce', 'GD': '#aa292b', 'GI': '#757575', 'GM': '#497e74', 'GMPV': '#dc3728', 'HS': '#258838',
+                   'NH': '#753f2f', 'NP': '#0f1e3d', 'OS': '#274b96', 'PS': '#932d75', 'SM': '#a03150', 'SSP': '#916b2a', 'SSS': '#6b4a08',
+                   'ST': '#75387a', 'TS': '#be5409'}
