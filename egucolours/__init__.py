144e8df94ef3fbfa4771e63b25151acbc68b9bb9
Andreas Kvas <andreas@kvas.at>
2022-11-19 22:28:17 +0100

    added formatted print output

--- egucolours/__init__.py
+++ egucolours/__init__.py
@@ -3,7 +3,9 @@
 class Colour(str):
     """
     Class representation of a colour. The colour is instantiated by its hex value, 
-    i.e. Color(name, hex_value).
+    i.e. Colour(name, hex_value). This class is designed to be compatible with the
+    matplotlib colour mechanics, but holds additional metadata in the form of the 
+    colour name.
 
     Properties
     ----------
@@ -17,9 +19,12 @@ class Colour(str):
         Red, green, blue primary values of the colour in the range 0.0 - 1.0.
 
     """
-    def __new__(self, name, hex_value):
+    def __new__(cls, name, hex_value):
+        return super(Colour, cls).__new__(cls, hex_value.lower())
+
+    def __init__(self, name, hex_value):
         self._name = name 
-        return super(Colour, self).__new__(self, hex_value.lower())
+        super().__init__()
 
     @property
     def name(self):
@@ -36,3 +41,7 @@ class Colour(str):
     @property
     def rgb(self):
         return tuple(int(self[i:i+2], 16) / 255 for i in (1, 3, 5))
+
+    def __str__(self):
+        a = self.RGB
+        return '{0:14s} RGB {1:3d} | {2:3d} | {3:3d}      ({4})'.format('EGU ' + self.name + ':', a[0], a[1], a[2], super().__str__())
