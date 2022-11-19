3171c28f9fd4661f51e297d4f64645a019471fb7
Andreas Kvas <andreas@kvas.at>
2022-11-19 22:30:45 +0100

    added modules for main, neutral and division colors

---
+++ egucolours/main.py
@@ -0,0 +1,8 @@
+from ._color_data import MAIN_PALETTE 
+from . import Colour, ColourMapping
+
+
+blue = Colour('blue', MAIN_PALETTE['blue'])
+yellow = Colour('yellow', MAIN_PALETTE['yellow'])
+
+colours = ColourMapping(MAIN_PALETTE)
---
+++ egucolours/neutral.py
@@ -0,0 +1,8 @@
+from ._color_data import NEUTRAL_PALETTE
+from . import Colour, ColourMapping
+
+
+white = Colour('white', NEUTRAL_PALETTE['white'])
+grey = Colour('grey', NEUTRAL_PALETTE['grey'])
+
+colours = ColourMapping(NEUTRAL_PALETTE)
